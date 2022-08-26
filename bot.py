ca5518288b28bb0fd1fca25d00c91b12fb8c062e
Bekzodbek <97502548+Bekzodbek2006@users.noreply.github.com>
2022-08-26 16:02:50 +0500

    commit

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
+from multiprocessing import context
+from turtle import update
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
 
 
 
-buttons = ReplyKeyboardMarkup([['help'],['admin']], resize_keyboard=True)
-my = ReplyKeyboardMarkup([['My nick name'],['My Username'],['My Id']], resize_keyboard=True)
+my = ReplyKeyboardMarkup([['My']], resize_keyboard=True)
+secret_btns = ReplyKeyboardMarkup([[ '2328' ], ['1225']])
 
 async def login(update, context):
     await update.message.reply_text(
-        f'Siz {update.effective_user.first_name} shu nom bilan kirdingiz ',reply_markup=buttons
+        f'Siz {update.effective_user.first_name} shu nom bilan kirdingiz'
     )
     return 1
 
@@ -18,37 +20,25 @@ async def start(update, context):
     )
 async def help(update, context):
     await update.message.reply_text(
-        f't.me/webdev06',reply_markup=buttons
+        't.me/webdev06'
     )
 
-async def admin(update, context):
-    await update.message.reply_text(
-        f'Adminlar @shoh_ake006 \n yana bittasi @webdev06',reply_markup=my
+async def my(update, context):
+    await update.massage.reply_text(
+        f' Sizning nick namegiz{update.effective_user.first_name} \n Sizning foydalanuvchi nomigiz @{update.effective_user.username} \n  Sizning id raqamingizni #{update.effective_user.id}'
     )
 
-async def usernick(update, context):
-    await update.message.reply_text(
-        f' Sizning nick namegiz{update.effective_user.first_name}',reply_markup=buttons
-    )
+async def secret2328(update, context):
+        await update.massage.reply_text(
+            'hello'
+        )
 
-async def username(update, context):
-    await update.message.reply_text(
-        f' Sizning foydalanuvchi nomigiz @{update.effective_user.username}',reply_markup=buttons
-    )
-
-async def id(update, context):
-    await update.message.reply_text(
-        f' Sizning id raqamingizni #{update.effective_user.id}',reply_markup=buttons
-    )
 conv_handler = ConversationHandler(
     entry_points=[CommandHandler("login", login)],
     states={
         1:{
-            MessageHandler(filters.Regex('^(help)$'),help),
-            MessageHandler(filters.Regex('^(admin)$'),admin),
-            MessageHandler(filters.Regex('^(My nick name)$'),usernick),
-            MessageHandler(filters.Regex('^(My Username)$'),username),
-            MessageHandler(filters.Regex('^(My Id)$'),id)
+            MessageHandler(filters.Regex('^(My)$'),my),
+            MessageHandler(filters=Regex('^(2328)$'),secret2328)
         }
        
     },
