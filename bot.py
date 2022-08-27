03b11056d2d1d6cd6c8b29c8b5121ddf2173eca3
bekzodbek2006 <bekzodbekk2006@gmail.com>
2022-08-27 18:55:11 +0500

    update bot

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
+
 from telegram import ReplyKeyboardMarkup, Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
-
+# from telegram.ext import *
+import datetime
+import time
 
 my = ReplyKeyboardMarkup([['My']], resize_keyboard=True)
 secret_btns = ReplyKeyboardMarkup([['2328'], ['1225']])
@@ -15,18 +18,71 @@ async def start(update, context):
     await update.message.reply_text(
         f'Salom {update.effective_user.first_name}'
     )
-async def help(update, context):
-    await update.message.reply_text(
-        't.me/webdev06'
-    )
 
 
+subxi = "04:10:00"
+bomdod = "05:00:00"
+quyosh = "05:35:00"
+qiyom = "12:20:00"
+peshin = "13:00:00"
+asr = "17:05:00"
+shom = "19:00:00"
+xufton = "21:40:00"
+
+async def obuna(update, context):
+    sor = True
+   
+    while sor == True:
+        x = datetime.datetime.now()
+        hozir = x.strftime("%H:%M:%S")
+        if hozir == subxi:
+            await update.message.reply_text(
+                f'Subxi boldi {hozir} \n Subxi {subxi}da!'
+            )
+        elif hozir == bomdod:
+            await update.message.reply_text(
+                f'Bomdod bo`ldi {hozir} \n Bomdod {bomdod}da!'
+            )
+        elif hozir == quyosh:
+            await update.message.reply_text(
+                f'Quyosh chiqmoqda ! {hozir} \n Quyosh Chqishi {quyosh}da!'
+            )
+        elif hozir == qiyom:
+            await update.message.reply_text(
+                f'Qiyom bo`ldi {hozir} \n Qiyom vaqti {qiyom}da!'
+            )
+        elif hozir == peshin:
+            await update.message.reply_text(
+                f'Peshin bo`ldi {hozir} \n Peshin {peshin}da!'
+            )
+        elif hozir == asr:
+            await update.message.reply_text(
+                f'Asr bo`ldi {hozir} \n Asr {asr}da!'
+            )
+        elif hozir == shom:
+            await update.message.reply_text(
+                f'Shom bo`ldi {hozir} \n Shom {shom}da!'
+            )
+        elif hozir == xufton:
+            await update.message.reply_text(
+                f'Xufton bo`ldi {hozir} \n Xufton {xufton}da!'
+            )
+        # else:
+        #     await update.message.reply_text(
+        #         f' {hozir} '
+        #     )
+        time.sleep(1)
+        
 async def myfun(update, context):
     await update.message.reply_text(
         f' Sizning nick namegiz{update.effective_user.first_name} \nSizning foydalanuvchi nomigiz @{update.effective_user.username} \nSizning id raqamingizni {update.effective_user.id}'
     )
 
 async def secret2328(update, context):
+        await update.message.reply_text(
+            'Siz adminmisiz? .... 😊'
+        )
+async def unknown(update, context):
         await update.message.reply_text(
             'Siz adminmisiz? .... 😊'
         )
@@ -44,11 +100,11 @@ conv_handler = ConversationHandler(
 )
 
 
-
-app = ApplicationBuilder().token("5401595436:AAHp9TQu5PjBXSea-9y6_j1t29O24zFON3s").build()
+app = ApplicationBuilder().token("5610525171:AAEPVr1e7n-0AVk0yXPyvUC0_AjkxL7Ew0g").build()
 
 
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("obuna", obuna))
 app.add_handler(conv_handler)
 
 app.run_polling()
